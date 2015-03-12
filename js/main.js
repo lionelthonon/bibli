88818cb7a370abc8d4ccef0bc7966e576987a4c2
Lionel Thonon <lionel.thonon@gmail.com>
2015-03-12 15:28:32 +0100

    End of the Day

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@ function mainFunction()
     {
         var banner = document.querySelector('.header-background.img');
         var nav =  document.getElementById('navigation');
+        var navContain = document.getElementById('nav-contain');
         banner.style.backgroundPosition = '0 '+(-window.pageYOffset/5)+'px';
-        if(pageYOffset >= 150) {
+        if(pageYOffset >= 95) {
             nav.style.padding = '0px';
-        }else if(pageYOffset < 150){
+            navContain.style.backgroundColor = 'rgba(43, 87, 151, 1)';
+        }else if(pageYOffset < 95){
             nav.style.padding = '15px';
+            navContain.style.backgroundColor = 'rgba(255, 255, 255, 0.3)';
         }
     }
 }
